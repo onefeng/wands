c35d40e6209f2d81ff22f9ce46737193bfe8469a
onefeng <15198086902@163.com>
2022-05-23 21:11:20 +0800

    feat: 处理接口

--- wands/server/views/search.py
+++ wands/server/views/search.py
@@ -5,11 +5,23 @@
 @time: 2022/5/23 11:29
 """
 
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+
+from wands import AddressParse
 
 search = Blueprint('search', __name__)
+
+address_parse = AddressParse()
+
+
+@search.route('/')
+def index():
+    return 'Welcome,wands!'
 
 
 @search.route('/address')
 def address():
-    return 'address'
+    area = request.args.get("area")
+    data = address_parse(area)
+    result = {'code': 200, 'data': data, 'message': 'success'}
+    return jsonify(result), 200
